b661422267250554c0a3f4b2294c8b8671829902
hazeezet <hazeezet@gmail.com>
2022-09-28 14:27:30 +0100

    init

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
     filename: '[name].js',
+	hashFunction: "xxhash64"
   },
   target: 'node',
   module: {
